85e813c059912b90af11c9822e57dba768e6ec45
Thomas von Deyen <thomas@vondeyen.com>
2017-01-12 13:31:45 +0100

    Clean up test setup
    We don't need to load all of this stuff.

--- spec/controllers/spree/admin/redirect_rules_controller_spec.rb
+++ spec/controllers/spree/admin/redirect_rules_controller_spec.rb
@@ -1,14 +1,19 @@
 require 'spec_helper'
 
-describe Spree::Admin::RedirectRulesController do
+RSpec.describe Spree::Admin::RedirectRulesController, type: :controller do
   # resourceful spree admin controller
-  specify{ described_class.ancestors.should include(Spree::Admin::ResourceController) }
+  specify do
+    expect(described_class.ancestors).to include(Spree::Admin::ResourceController)
+  end
 
-  specify { controller.send(:model_class).should == ::RedirectRule }
+  specify do
+    expect(controller.send(:model_class)).to eq ::RedirectRule
+  end
 
   describe 'build_resource' do
-    subject{ controller.send(:build_resource) }
-    it{ should be_a(RedirectRule) }
-    it{ should be_active }
+    subject { controller.send(:build_resource) }
+
+    it { is_expected.to be_a(RedirectRule) }
+    it { is_expected.to be_active }
   end
 end
